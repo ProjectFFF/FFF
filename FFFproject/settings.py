ac9334103637c5ae1e2f17d0eb331698641a9cd1
baaaam771 <jeongiun@naver.com>
2020-09-05 23:18:40 +0900

    [#1] solve css error

--- FFFproject/settings.py
+++ FFFproject/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'accounts/static'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
