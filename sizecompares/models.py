fe3caa482a556bde328b52abdd880cfc32264f84
baaaam771 <jeongiun@naver.com>
2020-09-25 05:07:51 +0900

    [#10 #17] sizecompare model

--- sizecompares/models.py
+++ sizecompares/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Compare(models.Model):
+    shoulder_compare = models.DecimalField(max_digits=6, decimal_places=3)
+    chest_compare = models.DecimalField(max_digits=6, decimal_places=3)
+    arm_compare = models.DecimalField(max_digits=6, decimal_places=3)
+    total_length_compare = models.DecimalField(max_digits=6, decimal_places=3)
