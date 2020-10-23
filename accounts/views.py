d822b68121b26b8d0bd7cd98c0ad7031cd2d79ca
baaaam771 <jeongiun@naver.com>
2020-10-24 02:01:39 +0900

    Member model & Read others closets

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from .models import Member
 
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user=User.objects.create_user(username=request.POST['id'], password=request.POST['password1'])
             auth.login(request, user)
+            member = Member()
+            member.id_m= request.POST['id']
+            member.password= request.POST['password1']
+            member.name= request.POST['name']
+            member.nickname= request.POST['nickname']
+            member.age= request.POST['age']
+            member.gender= request.POST['gender']
+            member.email= request.POST['email']
+            member.phone_naumber= request.POST['phone_number']
+            member.save()
+            
             return redirect('home')
     return render(request, 'signup.html')
 
